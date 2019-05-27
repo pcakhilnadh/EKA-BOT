b658f2db44ae8e16f43f612c42f94d2f6c6cc339
itzpc <mailtoitzpc@gmail.com>
2019-05-28 04:57:14 +0530

    welcome visior

--- bot.py
+++ bot.py
@@ -9,8 +9,14 @@ async def on_ready():
     print("BOt is ready")
 @client.event
 async def on_member_join(member):
-    channel = member.guild.system_channel
-    await channel.send('Elite Kerala Alliance you got a new member {0.mention}.'.format(member))
+    if member.guild.id == 561249245672374273:
+        welcomechannel = client.get_channel(582661044296744961)
+        embed = discord.Embed(
+            title = "Welcome!",
+            description = f"Hey {member.mention} welcome to {guild.name}!",
+            color = 0x07999b
+        )
+        await channel.send(embed = embed)
 @client.event
 async def on_member_remove(member):
     channel = member.guild.system_channel
