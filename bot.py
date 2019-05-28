896b50a783a1bac2e368ba6e518ed7e40ff0a2ee
itzpc <mailtoitzpc@gmail.com>
2019-05-28 06:37:11 +0530

    Welcome EKA

--- bot.py
+++ bot.py
@@ -3,26 +3,37 @@ import os
 from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
-
+'''
+def read_token():
+    with open("token.txt","r") as f:
+        lines=f.readlines()
+        return lines[0].strip() 
+'''
 @client.event
 async def on_ready():
     print("BOt is ready")
     
 @client.event
 async def on_member_join(member):
-    channel = member.guild.system_channel
-    if member.guild.id == 422681969210556416:
-        welcomechannel = client.get_channel(569025914604617729)
-        
+    if member.guild.id == 561249245672374273:
+        welcomechannel = client.get_channel(582661044296744961)
         embed = discord.Embed(
-            title = "Welcome!",
-            description = f"Hey {member.mention} welcome to {member.guild.name}!",
+            title = "Welcome",
+            description = f"Hey {member.mention} welcome to {member.guild.name} Server!",
             color = 0x07999b
         )
         await welcomechannel.send(embed = embed)
 @client.event
 async def on_member_remove(member):
-    channel = member.guild.system_channel
-    await channel.send('Elite Kerala Alliance you lost the member  {0}.'.format(member))
+    
+    if member.guild.id == 561249245672374273:
+        welcomechannel = client.get_channel(582661044296744961)
+        embed = discord.Embed(
+            title = "Bye Bye",
+            description = f"{member} left {member.guild.name} server!",
+            color = 0x07999b
+        )
+        await welcomechannel.send(embed = embed)
 
 client.run(os.environ.get('TOKEN')) #Token is env config var in Heroku Settings
+#client.run(read_token())
