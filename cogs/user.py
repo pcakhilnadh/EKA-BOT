bee5bea8075c70d553f9d20bfdb1af0215cd830b
itzpc <mailtoitzpc@gmail.com>
2019-06-19 01:03:58 +0530

    dm command

--- cogs/user.py
+++ cogs/user.py
@@ -28,7 +28,24 @@ class User(commands.Cog):
         embed.set_image(url = user.avatar_url) 
         
         await self.bot.get_channel(id=590236645442453544).send(embed=embed)
-        
+        await ctx.message.add_reaction("✅")
+
+    @commands.command(aliases=['Dm','dm','DM'])
+    async def dm_user(self, ctx, user:discord.User,msg:str):
+        """--> `eka dm @mentionUser message`"""
+        if user.bot:
+            return
+        embed = discord.Embed(title = f"You have a message from : {ctx.message.author}",
+            description = message
+            color = 0x98FB98
+            )
+        embed.set_image(url = ctx.message.author.avatar_url) 
+        try:
+            await ctx.message.author.send(embed=embed)
+        except:
+            await self.bot.get_channel(id=590236645442453544).send("The user has disabled DM.")
+        await ctx.message.add_reaction("✅")
+
         
 
 def setup(bot):
