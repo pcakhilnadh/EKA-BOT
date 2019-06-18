eb710e1e121aab09d90123f8814fe7f0dfe8989c
itzpc <mailtoitzpc@gmail.com>
2019-06-18 11:55:49 +0530

    bugs

--- cogs/owner.py
+++ cogs/owner.py
@@ -193,7 +193,7 @@ class Owner(commands.Cog):
                 messageObj=await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
                 if str(payload.emoji) == str("🔞"):
                     GuildObj = self.bot.get_guild(561249245672374273)
-                    roleObj = discord.utils.get(GuildObj.roles, name = "Member")
+                    roleObj = discord.utils.get(GuildObj.roles, name = "18+")
                     await memberObj.remove_roles(roleObj)
                     await self.bot.get_channel(id=590236645442453544).send(f"{memberObj.mention} Reacted to Remove `18+` Role ")
     @commands.Cog.listener()
