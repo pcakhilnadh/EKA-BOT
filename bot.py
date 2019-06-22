068afb38f0e6ea3aac40d3e29662fd093434ece4
itzpc <mailtoitzpc@gmail.com>
2019-06-22 19:07:09 +0530

    channels updated

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ class EkaBot(commands.AutoShardedBot):
         super().__init__(command_prefix=get_prefix, description=description)
         
         self.owner_id = 286367865462980608
-        self.channel_id= [586915160015503390,590236645442453544,562537063052738569,588736568597151760,590626344459698177]
+        self.channel_id= [586915160015503390,590236645442453544,562537063052738569,588736568597151760,590626344459698177,381963689470984203,591984765872111636]
         #bot_command, eka_bot,bot_test,votting_recruitment, longue
         self._task = self.loop.create_task(self.initialize())
         # self.spam_control = commands.CooldownMapping.from_cooldown(10, 12, commands.BucketType.user)
