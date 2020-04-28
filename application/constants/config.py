e811f8592bd3e856459fa9f0fc3bda89d3b48fdd
Akhil Nadh PC <mailtoitzpc@gmail.com>
2020-04-28 20:47:18 +0530

    typo corrected

--- application/constants/config.py
+++ application/constants/config.py
@@ -8,7 +8,7 @@ class DiscordConfig():
 
     #TOKEN = ""   local
     TOKEN = os.environ.get('TOKEN')
-    PREFIX = ['e ', 'EKA ', 'Eka ']
+    PREFIX = ['eka ', 'EKA ', 'Eka ']
     BOT_OWNER_ID =286367865462980608
     ALLOWED_CHANNELS= [Guild1947.BOT_TESTING_CHANNEL_ID,Guild1947.EKA_BOT_CHANNEL_ID,Guild1947.VOTING_FOR_RECRUIT_CHANNEL_ID,GuildSupport.COMMANDS_CHANNEL_ID,GuildSupport.BOT_COMMANDS_CHANNEL_ID ]
     ALLOWED_GUILDS = [ GuildSupport.SERVER_ID] 
